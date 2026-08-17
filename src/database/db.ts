cecc5b553829ffeaebbd2526dd75f6c932f5f484
joaar <joaquin.arias@uap.edu.ar>
2026-08-17 20:34:53 -0300

    chore: separar seed data de migrations (#30)

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,6 +1,5 @@
 import * as SQLite from 'expo-sqlite';
 import { runMigrations } from './migrations';
-import { runSeed } from './seed';
 
 let dbPromise: Promise<SQLite.SQLiteDatabase> | null = null;
 
@@ -12,11 +11,10 @@ export function getDb(): Promise<SQLite.SQLiteDatabase> {
       await database.execAsync('PRAGMA journal_mode = WAL;');
       console.log('[DB] running migrations...');
       await runMigrations(database);
-      if (__DEV__) {
-        console.log('[DB] __DEV__ = true, running seed...');
+      if (process.env.EXPO_PUBLIC_SEED_DB === 'true') {
+        console.log('[DB] seeding development data...');
+        const { runSeed } = await import('./seed');
         await runSeed(database);
-      } else {
-        console.log('[DB] __DEV__ = false, skipping seed');
       }
       return database;
     })();
